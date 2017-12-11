f0503f3fc0242ffbb453a1300bd83007e2234ed8
abutok <andrewbutok@ukr.net>
2017-12-11 12:55:59 +0200

    )

--- libft/includes/get_next_line.h
+++ libft/includes/get_next_line.h
@@ -13,7 +13,6 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include "libft.h"
-# include <fcntl.h>
 # include <errno.h>
 # define BUFF_SIZE 2000
 # define IS_EOL(x) (x == '\n' || x == '\0')
